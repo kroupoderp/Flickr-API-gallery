1ed3f90bc74312cdaf8213b8a716721a5626faca
Philip Kroupoderov <philip.kroupoderov@gmail.com>
2019-01-20 17:07:38 -0500

    Added a visit text in the overlay div container

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -20,17 +20,19 @@ class Image extends React.Component {
       console.log(imgHeight)
       if(imgHeight > 165) {
         this.overlay.style.height = '165px'
+        this.text.style.lineHeight = '165px'
       } else {
         this.overlay.style.height = imgHeight + 'px'
+        this.text.style.lineHeight = imgHeight + 'px'
       }
     }
   }
 
-  hover(e) {
+  hover() {
     this.setState({hover: true})
   }
 
-  leave(e) {
+  leave() {
     this.setState({hover: false})
   }
 
@@ -41,6 +43,7 @@ class Image extends React.Component {
         <React.Fragment>
           <img ref={ el => {this.image = el} } src={this.props.photo_url} alt=""/>
           <div ref={ el => {this.overlay = el } }  className="overlay" onMouseOut={this.leave}>
+          <div ref = { el => {this.text = el } } className="overlayText"><a href="https://www.flickr.com" className="text">visit</a></div>
           </div>
         </React.Fragment> :
 
