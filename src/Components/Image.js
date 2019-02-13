7c1fdf5cb57487e7330167bac155ec3b3ebc4898
Philip Kroupoderov <philip.kroupoderov@gmail.com>
2019-02-13 14:41:56 -0500

    added a method in the Image component that logs out the image number (index) when a user hovers over it

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -4,12 +4,35 @@ import PropTypes from 'prop-types';
 
 class Image extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      hovering: false
+    }
+  }
+
+  componentDidMount() {
+    let body = document.getElementsByTagName('body')[0]
+
+    let hoverHandler = (e) => {
+      if (parseInt(e.target.dataset.key) === this.props.label) {
+        console.log(e.target.dataset.key)
+      }
+    }
+
+    body.addEventListener('mouseover', hoverHandler)
+  }
+
+
+
   render() {
-    return (
-      <div style={this.props.styles}>
-        <img onLoad={this.props.onLoader} data-key={this.props.label} src={this.props.photo_url} alt=""/>
-      </div>
-    )
+    if (!this.state.hovering) {
+      return (
+        <div style={this.props.styles} className="image">
+          <img onLoad={this.props.onLoader} data-key={this.props.label} src={this.props.photo_url} alt=""/>
+        </div>
+      )
+    }
   }
 }
 
