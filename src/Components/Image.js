bfe2a2fca4e54cac708577fc35913ae6054d4ef7
Philip Kroupoderov <philip.kroupoderov@gmail.com>
2019-02-22 12:24:29 -0500

    code before working on the overlay feature for images

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -19,27 +19,23 @@ class Image extends React.Component {
   }
 
   render() {
-    if (!this.state.hovering) {
-      return (
-        <div onMouseOver={this.hoverHandler} style={this.props.styles} className="image">
-          <img onLoad={this.props.onLoader} src={this.props.photo_url} alt=""/>
-        </div>
-      )
-    } else {
-      // return the overlay div
-      // the filter style is here temporarily
-      return (
-        <div onMouseOver={this.hoverHandler} onMouseLeave={this.handleLeave} className="image">
-          <img style={{filter: 'opacity(20%)'}} onLoad={this.props.onLoader} data-key={this.props.label} src={this.props.photo_url} alt=""/>
-        </div>
-      )
-    }
+    return (
+      <div onMouseOver={this.hoverHandler} onMouseLeave={this.handleLeave} style={this.props.styles}>
+          <a href={this.props.origin}>
+            {this.state.hovering ? 
+              <img style={{filter: "opacity(20%)"}} onLoad={this.props.onLoader} src={this.props.photo_url} alt=""/>
+                :
+              <img onLoad={this.props.onLoader} src={this.props.photo_url} alt=""/>
+            } 
+          </a>
+      </div>
+    )
   }
 }
 
 Image.propTypes = {
   photo_url: PropTypes.string.isRequired,
-  // origin: PropTypes.string.isRequired,
+  origin: PropTypes.string.isRequired,
 };
 
 export default Image;
